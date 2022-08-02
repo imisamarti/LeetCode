f7facda3a362e1b9b033a9ba38cfcd335fed070f
Isadora Martinez <isadoramartinez123@gmail.com>
2022-08-01 21:00:14 -0400

    Time: 89 ms (51.95%), Space: 42.2 MB (62.61%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.js
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.js
@@ -3,28 +3,25 @@
  * @return {number}
  */
 var maximumWealth = function(accounts) {
+
+    let countW = 0;
     
-    //create a variable to hold maxWealth so far
-    let maxWealth = 0;
-    
-    //for loop to iterate through list of customers
-    for(let i = 0; i<accounts.length;i++){
-        //create variable to hold sum of accounts per customer
-        let sum = 0;
-        //create another loop to iterate though customers accounts
+    for(let i =0 ; i<accounts.length; i++){
+        let sum = 0
         
-        for(let j = 0; j<accounts[i].length;j++){
+        for(let j =0; j<accounts[i].length; j++){
             sum += accounts[i][j];
-            
-            
         }
         
-        //if the sum of the accounts is biggest than the current wealth then replace current wealth with sum
-        if(maxWealth < sum){
-            maxWealth = sum;
+        if(countW < sum){
+            countW = sum;
         }
+        
     }
     
-    return maxWealth
+    return countW
+    
     
 };
+
+
