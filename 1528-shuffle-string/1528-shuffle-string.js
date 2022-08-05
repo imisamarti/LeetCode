ea960076460d4863d83f7349b413f90ce505825d
Isadora Martinez <isadoramartinez123@gmail.com>
2022-08-04 20:25:35 -0400

    Time: 67 ms (97.15%), Space: 44.4 MB (60.85%) - LeetHub

---
+++ 1528-shuffle-string/1528-shuffle-string.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} s
+ * @param {number[]} indices
+ * @return {string}
+ */
+var restoreString = function(s, indices) {
+    
+    const str = [indices.length];
+    
+    for(let i = 0; i<indices.length; i++){
+      str[indices[i]] = s[i]
+    }
+    
+    return str.join('');
+};
