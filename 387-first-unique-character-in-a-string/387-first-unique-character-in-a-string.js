9705ba0e67a267bfa8198d2a5a2e99599c7f63e6
Isadora Martinez <isadoramartinez123@gmail.com>
2022-08-24 21:48:16 -0400

    Time: 78 ms (99.62%), Space: 44.6 MB (93.08%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.js
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.js
@@ -4,26 +4,13 @@
  */
 var firstUniqChar = function(s) {
     
-    let map  = new Map();
-    
-    for(let i =0; i<s.length;i++){
-        
-        let char = s.charAt(i);
-        
-        if(!map.has(char)){
-            map.set(char,1)
-        } else {
-            map.set(char,map.get(char)+1)
-        }
-    }
-    
-    for(let j =0; j<s.length;j++){
-        if(map.get(s.charAt(j)) == 1){
-            return j;
-        }
-        
-    }
-    
-        return -1;
-    
+   for(let i = 0;i<s.length;i++){
+       let char = s.charAt(i)
+       
+       if(s.indexOf(char) == i
+         && s.indexOf(char,i+1) == -1){
+           return i;
+       }
+   }
+    return -1;
 };
