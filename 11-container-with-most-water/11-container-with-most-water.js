ce5062816db8d2642a44cf8a2beff1e10c534255
Isadora Martinez <isadoramartinez123@gmail.com>
2022-08-27 19:32:28 -0400

    Time: 108 ms (60.47%), Space: 49.8 MB (49.85%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.js
+++ 11-container-with-most-water/11-container-with-most-water.js
@@ -4,25 +4,23 @@
  */
 var maxArea = function(height) {
     
-    let left = 0;
-    let right = height.length-1;
-    let result = 0;
+    
+let     left = 0,
+        right = height.length-1,
+        result = 0;
     
     while(left<right){
+        let currentMaxHeightContainer = Math.min(height[left],height[right])
+        let area = currentMaxHeightContainer *(right-left);
         
-    let h = Math.min(height[left], height[right]);
-    let area = h*(right-left);
+        result = Math.max(area,result)
         
-    result = Math.max(area, result);
-        
-        if(height[left] < height[right]){
-            left++
-        } else {
-            right--
-        }
-        
+        if(height[left]<height[right])left ++
+        else right--
     }
     
     return result;
+    
+    
     
 };
